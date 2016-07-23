881a553f60e694d213ea1f13c0a31030bfcbe60b
Hyunkyu Lee <hkl0902@gmail.com>
2016-07-23 16:46:27 -0700

    Adds randomly created cells for testing

--- BatchEditing/BatchSelectionTableViewController.swift
+++ BatchEditing/BatchSelectionTableViewController.swift
@@ -29,23 +29,26 @@ class BatchSelectionTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        // Configure the cell...
+        if let batchCell = cell as? BatchSelectionTableViewCell {
+            let id = NSUUID().UUIDString
+            batchCell.nameLabel.text = id.substringToIndex(id.startIndex.advancedBy(4))
+        }
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
