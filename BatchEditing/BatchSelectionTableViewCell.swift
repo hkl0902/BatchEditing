220806156d79ad402a4381606d391516b8f3945f
Hyunkyu Lee <hkl0902@gmail.com>
2016-07-23 16:39:30 -0700

    Sets up basic files for the TVC and Cell

---
+++ BatchEditing/BatchSelectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BatchSelectionTableViewCell.swift
+//  BatchEditing
+//
+//  Created by Hyunkyu Lee on 7/23/16.
+//  Copyright © 2016 Hyunkyu Lee. All rights reserved.
+//
+
+import UIKit
+
+class BatchSelectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
